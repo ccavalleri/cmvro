b0114b1096108160b9512ccdc9324b764b5cfa36
bacetech <ciro.cavalleri@gmail.com>
2023-01-10 14:07:42 +0100

    installing next.js, tailwind css, heroicons, and header boilerplate

---
+++ components/Header.js
@@ -0,0 +1,92 @@
+import { SearchIcon, AdjustmentsVerticalIcon } from '@heroicons/react/24/solid';
+import { useRef } from 'react';
+
+export default function Header() {
+  const searchInputRef = useRef(null);
+  return (
+    <header className="py-3 lg:py-0 bg-white shadow-md mdk-header--fixed">
+      <div className="lg:pl-4 px-8 flex flex-wrap items-center">
+        <div className="items-center">
+          <a
+            href="/"
+            className="flex items-center text-xl w-sidebar mr-4 hover:no-underline"
+          >
+            <span className="mr-1 -mt-1 text-primary"></span>
+            <span>
+              <strong>CMVRO</strong>
+            </span>
+          </a>
+        </div>
+
+        <div className="flex-1 flex justify-between">
+          <div className="relative text-gray-darker hidden lg:flex flex-grow">
+            <div className="flex w-full mt-2 mb-2 mx-auto max-w-[90%] border border-gray-200 hover:shadow-lg focus-within:shadow-lg px-2 py-3 rounded-full items-center sm:max-w-xl lg:max-w-2xl">
+              <SearchIcon className="h-5 text-gray-500 mr-3" />
+              <input
+                ref={searchInputRef}
+                type="text"
+                className="flex-grow focus:outline-none"
+              />
+              <AdjustmentsVerticalIcon className="h-5" />
+            </div>
+            <button
+              type="submit"
+              className="absolute right-0 top-0 mt-3 mr-4"
+            ></button>
+          </div>
+        </div>
+
+        <label
+          data-toggle="sidebar"
+          className="pointer-cursor lg:hidden block"
+        ></label>
+
+        <div className="lg:flex lg:items-center lg:w-auto w-full" id="menu">
+          <ul className="lg:flex items-center justify-between text-md text-gray-900 font-semibold pt-4 lg:pt-0">
+            <li className="relative inline-block text-left">
+              <a
+                href="#"
+                className="flex items-center lg:p-4 py-3 px-0 block hover:text-blue-600 font-semibold"
+                id="options-menue1"
+                aria-haspopup="true"
+                aria-expanded="true"
+              >
+                Features
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center lg:p-4 py-3 px-0 block hover:text-blue-600 font-semibold"
+                id="options-menue"
+                aria-haspopup="true"
+                aria-expanded="true"
+              >
+                Pricing
+              </a>
+            </li>
+            <li></li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center lg:p-4 py-3 px-0 block hover:text-blue-600 font-semibold"
+                id="options-menue2"
+                aria-haspopup="true"
+                aria-expanded="true"
+              >
+                Sign in
+              </a>
+            </li>
+
+            <li className="mr-4 text-muted">or</li>
+            <li>
+              <a className="btn btn-success" href="login.html">
+                Login
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+}
