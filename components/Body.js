16fa92e6a6e1295444a7c6fac1b5c1008a4c6756
bacetech <ciro.cavalleri@gmail.com>
2023-01-15 22:11:21 +0100

    reverse change

--- components/Body.js
+++ components/Body.js
@@ -1,9 +1,7 @@
 export default function Body() {
   return (
     <div>
-      <p className="text-gray-700 text-3xl mb-16 font-bold">
-        Quanti mi vuoi ben?
-      </p>
+      <p className="text-gray-700 text-3xl mb-16 font-bold">Dashboard</p>
       <div className="grid lg:grid-cols-3 gap-5 mb-16">
         <div className="rounded bg-white h-56 shadow-lg"></div>
         <div className="rounded bg-white h-56 shadow-lg"></div>
