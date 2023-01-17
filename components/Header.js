78c6bada5ee28dbe328170926bf49879e26db8f1
bacetech <ciro.cavalleri@gmail.com>
2023-01-17 17:14:43 +0100

    hide dropdown

--- components/Header.js
+++ components/Header.js
@@ -40,7 +40,7 @@ export default function Header() {
 
           <div class="flex justify-center">
             <div>
-              <div class="dropdown relative">
+              <div class="dropdown relative hidden">
                 <button
                   className="dropdown-toggle px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 
                   active:bg-blue-800 active:shadow-lg active:text-white transition duration-150 ease-in-out flex items-center whitespace-nowrap"
