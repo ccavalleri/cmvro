6af88cb7b5839ae35de9bd459241fd410a28141d
bacetech <ciro.cavalleri@gmail.com>
2023-02-02 10:51:31 +0100

    styling adjustment

--- pages/Operator/Inprocessing.js
+++ pages/Operator/Inprocessing.js
@@ -19,20 +19,23 @@ export default function Inprocessing() {
       <p className="text-gray-700 text-3xl mb-4 font-bold">In processing</p>
       <form>
         <Body
-          content={<>
-            <div className="flex grid grid-cols-2 gap-2 mt-2 h-fit">
-              <Inputfield placeholder="Last Name" />
-              <Inputfield placeholder="First Name" />
-              <Inputfield placeholder="DODID" />
-              <Inputfield placeholder="Date of Birth" />
-              <Inputfield placeholder="Nationality" />
-              <Inputfield placeholder="Service" />
-              <Inputfield placeholder="Command" />
-              <Inputfield placeholder="Installation" />
-              <Inputfield placeholder="Rank" />
-              <Inputfield placeholder="Rating" />
-            </div>
-            <button className="md:px-16 px-4 mt-2 ml-2 mb-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Save</button>
+          content={
+            <>
+              <div className="flex grid grid-cols-2 gap-2 mt-2 h-fit">
+                <Inputfield placeholder="Last Name" />
+                <Inputfield placeholder="First Name" />
+                <Inputfield placeholder="DODID" />
+                <Inputfield placeholder="Date of Birth" />
+                <Inputfield placeholder="Nationality" />
+                <Inputfield placeholder="Service" />
+                <Inputfield placeholder="Command" />
+                <Inputfield placeholder="Installation" />
+                <Inputfield placeholder="Rank" />
+                <Inputfield placeholder="Rating" />
+              </div>
+              <button className="md:px-16 px-4 mt-4 ml-2 mb-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                SAVE
+              </button>
             </>
           }
         />
