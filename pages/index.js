0c655628eca967ef23c721da3ce1362835f1559b
bacetech <ciro.cavalleri@gmail.com>
2023-02-02 10:53:23 +0100

    remove extra space from home

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Dashboard from './Viewer/Dashboard';
 export default function Home() {
   return (
     <div>
-      <div className="px-4 md:px-16">
+      <div className="">
         <Dashboard />
       </div>
     </div>
