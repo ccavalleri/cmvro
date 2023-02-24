d98e2110ace7415a68742bd046653cb946b0d93a
bacetech <ciro.cavalleri@gmail.com>
2023-02-24 11:37:32 +0100

    fixing an issue

--- pages/Operator/PersonnelnVehicles.js
+++ pages/Operator/PersonnelnVehicles.js
@@ -397,49 +397,19 @@ export default function PersonnelnVehicles() {
                     id="dashboard"
                     role="tabpanel"
                     aria-labelledby="dashboard-tab"
-                  >
-                    <p className="text-sm text-gray-500 dark:text-gray-400">
-                      This is some placeholder content the{' '}
-                      <strong className="font-medium text-gray-800 dark:text-white">
-                        Dashboard tab's associated content
-                      </strong>
-                      . Clicking another tab will toggle the visibility of this
-                      one for the next. The tab JavaScript swaps classes to
-                      control the content visibility and styling.
-                    </p>
-                  </div>
+                  ></div>
                   <div
                     className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800"
                     id="settings"
                     role="tabpanel"
                     aria-labelledby="settings-tab"
-                  >
-                    <p className="text-sm text-gray-500 dark:text-gray-400">
-                      This is some placeholder content the{' '}
-                      <strong className="font-medium text-gray-800 dark:text-white">
-                        Settings tab's associated content
-                      </strong>
-                      . Clicking another tab will toggle the visibility of this
-                      one for the next. The tab JavaScript swaps classes to
-                      control the content visibility and styling.
-                    </p>
-                  </div>
+                  ></div>
                   <div
                     className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800"
                     id="contacts"
                     role="tabpanel"
                     aria-labelledby="contacts-tab"
-                  >
-                    <p className="text-sm text-gray-500 dark:text-gray-400">
-                      This is some placeholder content the{' '}
-                      <strong className="font-medium text-gray-800 dark:text-white">
-                        Contacts tab's associated content
-                      </strong>
-                      . Clicking another tab will toggle the visibility of this
-                      one for the next. The tab JavaScript swaps classes to
-                      control the content visibility and styling.
-                    </p>
-                  </div>
+                  ></div>
                 </div>
               </div>
             </div>
