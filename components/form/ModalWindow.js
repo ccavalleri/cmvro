ced30d1ef95ff475424a038a04611eaf467bf72e
bacetech <ciro.cavalleri@gmail.com>
2023-03-13 18:21:51 +0100

    adding changes to the modal view

--- components/form/ModalWindow.js
+++ components/form/ModalWindow.js
@@ -152,8 +152,8 @@ export default function ModalWindow({ show, onCloseDismiss }) {
           </div>
         </div>
       </Modal.Body>
-      <Modal.Footer>
-        <div className="flex justify-end">
+      <Modal.Footer className="flex justify-end">
+        <div>
           <button
             onClick={onCloseDismiss}
             className="font-bold focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-lg px-5 py-2 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
