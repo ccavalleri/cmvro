2a56a7f3e3ec1cc11669d841512d681112cf7273
bacetech <ciro.cavalleri@gmail.com>
2023-04-02 20:32:11 +0200

    sync with production

--- pages/admin/Operator/Waiting.js
+++ pages/admin/Operator/Waiting.js
@@ -3,7 +3,7 @@ import Body from '../../../components/Body';
 export default function Waiting() {
   return (
     <div className="px-4 md:px-16">
-      <p className="text-gray-700 text-3xl mb-4 font-bold">Waiting List</p>
+      <p className="text-gray-700 text-3xl mb-4 font-bold">Appointment List</p>
       <Body />
     </div>
   );
