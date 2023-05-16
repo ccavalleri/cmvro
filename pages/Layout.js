661fc3febb356e5d7c437aa970eef94c375021a6
bacetech <ciro.cavalleri@gmail.com>
2023-05-16 18:56:29 +0200

    updating landing page

--- pages/Layout.js
+++ pages/Layout.js
@@ -706,7 +706,31 @@ export default function Layout({ children }) {
               >
                 <p className="w-full text-gray-600 text-xs md:text-sm px-6"></p>
                 <div className="w-full font-bold text-xl text-gray-800 px-6">
-                  Driver License Information
+                 Plan an appointment
+                </div>
+                <p className="text-gray-800 text-base px-6 mb-5">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.
+                </p>
+              </a>
+            </div>
+            <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
+              <div className="flex items-center justify-end">
+                <button className="mx-auto lg:mx-0 hover:underline gradient text-gray font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                  Action
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+            <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+              <a
+                href="#"
+                className="flex flex-wrap no-underline hover:no-underline"
+              >
+                <p className="w-full text-gray-600 text-xs md:text-sm px-6"></p>
+                <div className="w-full font-bold text-xl text-gray-800 px-6">
+                  Dependent Management
                 </div>
                 <p className="text-gray-800 text-base px-6 mb-5">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -730,31 +754,7 @@ export default function Layout({ children }) {
               >
                 <p className="w-full text-gray-600 text-xs md:text-sm px-6"></p>
                 <div className="w-full font-bold text-xl text-gray-800 px-6">
-                  Driver License Information
-                </div>
-                <p className="text-gray-800 text-base px-6 mb-5">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.
-                </p>
-              </a>
-            </div>
-            <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-              <div className="flex items-center justify-end">
-                <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                  Action
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
-            <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
-              <a
-                href="#"
-                className="flex flex-wrap no-underline hover:no-underline"
-              >
-                <p className="w-full text-gray-600 text-xs md:text-sm px-6"></p>
-                <div className="w-full font-bold text-xl text-gray-800 px-6">
-                  Driver License Information
+                 Road Tax Payment
                 </div>
                 <p className="text-gray-800 text-base px-6 mb-5">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
